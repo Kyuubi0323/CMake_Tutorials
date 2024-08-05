c3310af0f326d4d4165751bf555b0600a0cf6d75
Kyuubi <khoi.nv202647@sis.hust.edu.vn>
2024-08-05 22:09:58 +0700

    _add ex1_step1

---
+++ Step1/Ex1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main()
+{
+    int a = 2;
+    int b = 3;
+    int c = 0;
+    c = a + b;
+    printf("Hello Guys\n");
+    printf("Value: %d\n", c);
+    system("pause");
+}
